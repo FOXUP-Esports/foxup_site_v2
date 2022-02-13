2764b24e6204b989f9e85ce5af59396ae60871c9
Patrice MALDI <patrice.maldi@gmail.com>
2022-02-14 00:36:30 +0100

    feat: add multiples news pages and On live button

---
+++ news_page.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact | FOXUP Esports</title>
+
+    <?php include 'components/head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="css/news_page.css" media="all" />
+
+</head>
+
+<body>
+    <?php include 'components/nav.php'; ?>
+    <main>
+
+        <div class="spacer"></div>
+
+        <!--  Start News Page  -->
+        <section id="news_page">
+            <div class="container">
+                <div class=image-news>
+                    <img src="https://static.wixstatic.com/media/ab3530_30001e69e69d4aa6b16133e9ffdddad0~mv2.jpg" class="img-fluid">
+                </div>
+                <div class="title-news">
+                    <p>Notre coach PtiRenard qui explique pourquoi Fracture est difficile à jouer.</p>
+                </div>
+                <div class="body-news">
+                    <p>Fracture est une map compliqué à appréhender par les joueurs de par sa construction en "H" qui la rend unique. En effet, les défenseurs spawn au milieu de la map quant aux attaquants, ils spawn en bas de la map mais peuvent très rapidement se trouver en haut par la tyrolienne centrale. 
+La map est faite de tel sorte a ce que la défense se fasse vite submergé par l'attaque qui peux les prend sur deux front opposé. La défense doit donc trouver des solutions pour comprendre les mouvements de l'attaque pour réagir en conséquences le plus rapidement possible. 
+
+Une des solutions envisageable serais donc de jouer agressifs sur certaines zones neutres de la map tout en ayant une vision de ce qui se passe de l'autre coté. Cela permet aussi de cycler les ultimate de chaque agent puisqu'il y a quatre orbes dans la map. 
+
+Pour finir, je parlerais des compositions. 
+Aujourd'hui, c'est compliqué de sortir une composition type mais on peux voir certains agents être mis en avant sur les différentes compétitions. Tout d'abord Viper, grâce a son kit, elle permet de bloquer des zones et dissuader ses adversaires de passer au travers.
+Ceci implique un second smokeur qui peux être Astra ou bien Omen. Une sage serais aussi la bien venu, encore une fois pour bloquer une zone avec son wall et donc gagner de précieuse seconde pour prendre un site ou le garder. 
+Pour le rôle de duelliste, Jett reste toujours l'agent favori même si je pense qu'une Raze peut être la bienvenue grâce aux endroits étroits de la carte. Un Breach peut être bien pour totalement anéantir les possibilité de Killjoy, même si je pense que Cypher seras plus utile pour garder des zones. 
+
+Si les joueurs veulent jouer une compo plus agressives, la map permet d'introduire un second duelliste, qui comme icebox, pourrais très bien être une Reyna. 
+
+Je vous laisse donc choisir ce qu'il seras le plus adapter pour mener a bien votre mission, devenir un Valorant.
+
+</p>
+                </div>
+            </div>
+        </section>
+        <!-- End News Page  -->
+
+        <?php include('components/footer.php'); ?>
+
+    </main>
+</body>
+
+</html>
