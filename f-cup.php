182345ce4d1c4ed979f5cec72b09ecd19a2f8d60
LaCh3vre <pascal-lecalve@laposte.net>
2022-02-15 14:06:34 +0100

    f-cup

---
+++ f-cup.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>F-Cup | FOXUP Esports</title>
+
+    <?php include 'components/head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="css/f-cup.css" media="all" />
+
+</head>
+
+<body>
+    <?php include 'components/nav.php'; ?>
+    <main>
+
+        <div class="spacer"></div>
+
+        <!--  Start Contact  -->
+        <section id="F-CUP">
+            <div class="container">
+                <img src="image/baniereFCUP.png" class="img-fluid" />
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6 col-xl-6">
+                        <div>
+                            <img src="image/image f-cup kiiperone.webp" height="300px" width="300px" />
+                        </div>
+                    </div>
+                    <div class="col-6 text-center">
+                        <h3>INSCRIPTION F-CUP <br />EDITION</h3>
+                        <p>_________________ <br />15,00€</p>
+                        <div>
+                            <a class="arrondi" href="detailfcup.php">Affichier les détails</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Contact  -->
+
+        <div class="spacer"></div>
+
+        <?php include('components/footer.php'); ?>
+
+    </main>
+</body>
+
+</html>
